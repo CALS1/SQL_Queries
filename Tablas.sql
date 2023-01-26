bd8df3f1674bb58499bf86849f8e8131e6ea8ac1
Camilo <123599777+CALS1@users.noreply.github.com>
2023-01-25 19:48:06 -0500

    Add files via upload

---
+++ Tablas.sql
@@ -0,0 +1,69 @@
+-- Creación de tablas de Abdel
+-- Curso SQL
+CREAR  MESA  platzi .carreras (
+	identificación INT ,
+	carrera VARCHAR ( 100 ),
+	fecha_alta TIMESTAMP ,
+	vigente BOOLEAN
+);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 1 , ' Negocios y administración ' , ' 2017-05-28 06:42:45 ' , VERDADERO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 2 , ' Administración y gestión de empresas ' , ' 2016-03-24 13:52:17 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 3 , ' Contabilidad y fiscalización ' , ' 2018-10-29 18:33:57 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 4 , ' Finanzas, banca y seguros ' , ' 2020-05-11 02:05:57 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 5 , ' Mercadotecnia y publicidad ' , ' 2020-03-20 23:53:23 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 6 , ' Negocios y administración, programas multidisciplinarios o generales ' , ' 2019-02-25 09:02:59 ' , VERDADERO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 7 , ' Negocios y comercio ' , ' 2015-08-06 16:21:29 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 8 , ' Ciencias sociales y estudios del comportamiento ' , ' 2018-05-24 10:38:58 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 9 , ' Ciencias políticas ' , ' 2019-07-23 23:01:06 ' , VERDADERO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 10 , ' Economía ' , ' 2015-01-02 15:41:37 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 11 , ' Psicología ' , ' 2014-11-23 02:07:16 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 12 , ' Sociología y antropología ' , ' 2019-12-28 21:12:40 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 13 , ' Trabajo y atención social ' , ' 2020-01-04 19:42:57 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 14 , ' Ciencias de la información ' , ' 2019-04-01 16:28:38 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 15 , ' Comunicación y periodismo ' , ' 2015-07-30 23:55:18 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 16 , ' Derecho ' , ' 2016-03-01 13:23:43 ' , VERDADERO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 17 , ' Criminología ' , ' 2018-07-20 21:34:08 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 18 , ' Derecho ' , ' 2019-03-20 04:41:15 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 19 , ' Ingeniería, manufactura y construcción ' , ' 2017-07-09 06:15:00 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 20 , ' Arquitectura y construcción ' , ' 2016-01-12 23:25:38 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 21 , ' Arquitectura y urbanismo ' , ' 2017-04-24 21:01:50 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 22 , ' Construcción e ingeniería civil ' , ' 2018-07-30 18:21:47 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 23 , ' Ingeniería industrial, mecánica, electrónica y tecnología ' , ' 2018-05-13 00:11:20 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 24 , ' Electricidad y generación de energía ' , ' 2016-06-26 12:37:15 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 25 , ' Electrónica y automatización ' , ' 2016-10-04 12:20:15 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 26 , ' Ingeniería de vehículos de motor, barcos y aeronaves ' , ' 2020-02-02 17:06:53 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 27 , ' Ingeniería industrial, mecánica, electrónica y tecnología, programas multidisciplinarios o generales ' , ' 2019-10-04 07:03:01 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 28 , ' Ingeniería mecánica y metalurgia ' , ' 2017-04-15 17:00:21 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 29 , ' Ingeniería química ' , ' 2015-05-16 07:41:17 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 30 , ' Tecnología y protección del medio ambiente ' , ' 2018-08-25 11:06:31 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 31 , ' Tecnologías de la información y comunicación ' , ' 2016-05-05 11:48:52 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 32 , ' Manufacturas y procesos ' , ' 2018-05-14 12:28:54 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 33 , ' Industria de la alimentación ' , ' 2015-09-07 23:38:43 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 34 , ' Manufacturas y procesos, programas multidisciplinarios o generales ' , ' 2019-04-12 04:01:36 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 35 , ' Minería y extracción ' , ' 2016-11-25 02:59:50 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 36 , ' Educación ' , ' 2016-01-28 16:16:17 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 37 , ' Formación docente ' , ' 2016-06-05 01:51:20 ' , VERDADERO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 38 , ' Bellas artes ' , ' 2018-02-22 07:08:23 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 39 , ' Formación docente para educación básica, nivel preescolar ' , ' 2016-07-01 11:40:06 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 40 , ' Formación docente para educación básica, nivel primario ' , ' 2018-07-16 17:01:05 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 41 , ' Formación docente para educación básica, nivel secundario ' , ' 2015-10-31 01:50:38 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 42 , ' Formación docente para educación física, artística o tecnológica ' , ' 2020-03-06 22:44:55 ' , VERDADERO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 43 , ' Formación docente para la enseñanza de asignaturas específicas ' , ' 2015-02-22 09:57:20 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 44 , ' Formación docente para otros servicios educativos ' , ' 2020-05-13 20:41:47 ' , VERDADERO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 45 , ' Formación docente, programas multidisciplinarios o generales ' , ' 2016-08-27 03:23:27 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 46 , ' Educación ' , ' 2019-05-08 16:40:22 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 47 , ' Ciencias de la educación, programas multidisciplinarios o generales ' , ' 2015-12-25 23:21:15 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 48 , ' Didáctica, pedagogía y currículo ' , ' 2014-09-10 11:14:36 ​​' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 49 , ' Orientación y asesoría educativa ' , ' 2020-03-21 08:30:22 ' , VERDADERO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 50 , ' Ciencias naturales, exactas y de la computación ' , ' 2018-05-29 18:40:27 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 51 , ' Ciencias naturales ' , ' 2016-04-21 05:20:07 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 52 , ' Biología y bioquímica ' , ' 2017-05-24 18:25:19 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 53 , ' Ciencias ambientales ' , ' 2019-09-08 19:34:34 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 54 , ' Ciencias de la computación ' , ' 2018-12-29 06:01:08 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 55 , ' Ciencias de la computación ' , ' 2020-03-08 16:23:46 ' , TRUE);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 56 , ' Ciencias físicas, químicas y de la tierra ' , ' 2016-09-19 13:05:30 ' , VERDADERO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 57 , ' Ciencias de la tierra y la atmósfera ' , ' 2018-12-16 00:52:43 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 58 , ' Física ' , ' 2019-01-12 05:56:02 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 59 , ' Química ' , ' 2020-07-15 06:50:54 ' , FALSO);
+INSERTAR EN  platzi . carreras (id, carrera, fecha_alta, vigente) VALORES ( 60 , ' Matemáticas y estadística ' , ' 2020-06-06 16:40:34 ' , VERDADERO);
+
